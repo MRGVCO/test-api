af04835902e33afbab299270dcb5bf1359845c1f
Mike <mikegarite@gmail.com>
2021-02-07 17:51:58 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST || 'otto.db.elephantsql.com',
+    host: env.DB_HOST || 'http://getreal.cxgbtkv0eyak.us-west-2.rds.amazonaws.com',
     port: env.DB_PORT || '5432',
-    user: env.DB_USER || 'cklijfef',
-    password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
-    database: env.DB_NAME || 'cklijfef',
+    user: env.DB_USER || 'postgres',
+    password: env.DB_PASSWORD || 'gomrgvgo!!77',
+    database: env.DB_NAME || 'template1',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
